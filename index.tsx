a7cd7e483bd61ced47b787fc6daf47c77ace6493
Ollie <33800208+wawz@users.noreply.github.com>
2022-04-15 13:59:42 +0800

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import HomePage from '../components/home-page'
+import HomePage from '@components/home-page'
 
 const Home: NextPage = () => {
   return <HomePage />
