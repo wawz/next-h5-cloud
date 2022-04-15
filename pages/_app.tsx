e70e4b79de06d3f378f3785fb31d4e86436a3092
Ollie <33800208+wawz@users.noreply.github.com>
2022-04-15 11:32:02 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,10 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
       </Head>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
