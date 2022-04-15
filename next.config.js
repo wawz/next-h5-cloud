7eb513f9d5fbb1cc969967a0f824d1d61b32ab90
Ollie <33800208+wawz@users.noreply.github.com>
2022-04-15 14:38:22 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -10,30 +10,6 @@ module.exports = withImages({
   images: {
     domains: ['clarins-te.oss-cn-hongkong.aliyuncs.com'],
   },
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.(mp3|mp4)$/i,
-      exclude: config.exclude,
-      use: [
-        {
-          loader: require.resolve('url-loader'),
-          options: {
-            limit: -1,
-            publicPath: `/_next/static/media/`,
-            outputPath: `${isServer ? '../' : ''}static/media/`,
-            name: '[name]-[hash:8].[ext]',
-            esModule: config.esModule || false
-          }
-        }
-      ]
-    });
-    return config;
-  },
-  i18n: {
-    locales: ['sc-tr', 'en-tr', 'fr-tr'],
-    defaultLocale: 'sc-tr',
-    localeDetection: false
-  },
   headers: async () => {
     return [
       {
@@ -52,6 +28,5 @@ module.exports = withImages({
   },
   webpack5: true,
   poweredByHeader: false,
-  compress: false,
   generateEtags: false
 })
