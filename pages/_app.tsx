fe5864d0844bde59aaff4eec9b67e0fb2ae12c41
Ollie <33800208+wawz@users.noreply.github.com>
2022-04-20 18:07:44 +0800

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <title>clarins</title>
+        <title>CLARINS Treatment Essences</title>
       </Head>
       <Component {...pageProps} />
       {/* google analytics */}
